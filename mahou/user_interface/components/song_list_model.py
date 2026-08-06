03af3b53214898fc7c6b0d3e6cfc66b5e9d49e38
Ezu1502 <ezu150208@gmail.com>
2026-08-06 11:34:20 -0300

    Building abstract model

--- mahou/user_interface/components/song_list_model.py
+++ mahou/user_interface/components/song_list_model.py
@@ -1,5 +1,7 @@
 from PySide6.QtCore import QAbstractListModel, Qt, QModelIndex
 from mahou.core.song import Song
+from typing import Literal
+from contextlib import contextmanager
 
 Roles = Qt.ItemDataRole
 
@@ -16,7 +18,7 @@ class SongListModel(QAbstractListModel):
 
     def data(self, index, role = Roles.DisplayRole):
         song = self.song_list[index.row()]
-        
+
         if role == Roles.DisplayRole:
             return song.title
         elif role == Roles.UserRole:
@@ -26,3 +28,28 @@ class SongListModel(QAbstractListModel):
 
     def get_song_from_index(self, index: int) -> Song:
         return self.song_list[index]
+
+    def update_list_order(self, key: Literal["title", "id", "play_count"]):
+        self.song_list.sort(key = lambda song: getattr(song, key))
+
+    @contextmanager
+    def reset_model(self):
+        self.beginResetModel()
+
+        try:
+            yield
+        finally:
+            self.endResetModel()
+
+
+            #! CONTINUAR DAQUI !!!!!!!!!!!!!1
+
+
+            
+    def set_songs(self, song):
+        self.beginResetModel()
+
+        self.song_list = list(self.song_map.values()) # ! terminar ainda, isso é um placeholder
+
+        self.endResetModel()
+
